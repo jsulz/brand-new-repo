401c1c8a259080a4f0a24219259a732a5b3c7f70
jsulz <j.sulzdorf@gmail.com>
2022-12-02 14:18:02 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Pantheon Gastby Running Logs
+
+## Welcome
+
+This is a running log of my experiences with the Gastby SDK built with love by the Pantheon team. To see the original repository: [Pantheon Decoupled Kit Gastby WordPress Starter](https://github.com/pantheon-systems/gatsby-wordpress-starter)
+
+## Startup
+
+To run:
+
+1. Clone this repo
+
+2. Install node modules
+
+```bash
+cd decoupled-wp-gatsby-frontend-demo && npm install
+```
+
+3. Save `.env.example` as `.env.local` add/update following lines:
+
+```
+WPGRAPHQL_URL=
+```
+
+4. Run `npm run develop`
+
+5. Open a browser and navigate to `http://localhost:8000`.
+
+6. Added basic auth
---
+++ src/components/layout.js
@@ -0,0 +1,65 @@
+import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import parse from "html-react-parser"
+import Footer from "./footer"
+const Layout = ({ isHomePage, children }) => {
+  const {
+    wp: {
+      generalSettings: { title, description },
+    },
+  } = useStaticQuery(graphql`
+    query LayoutQuery {
+      wp {
+        generalSettings {
+          title
+          description
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="min-h-screen flex flex-col min-w-fit">
+      <div
+        className="my-0 mx-auto py-10 px-5 max-w-screen-sm"
+        data-is-root-path={isHomePage}
+      >
+        <header className="text-lg mb-4">
+          <nav>
+            <ul className="flex flex-row flex-wrap sm:flex-nowrap list-none justify-between">
+              {[
+                ["🏠 Home", "/"],
+                ["📑 Pages", "/pages"],
+              ].map(([link, to], i) => (
+                <li className="my-auto" key={`key-${i}`}>
+                  {/* if we're on the homepage, render site title */}
+                  {to === "/" && isHomePage ? (
+                    <div>
+                      <h1 className="my-auto">
+                        <Link className="font-bold text-black text-4xl" to="/">
+                          {parse(title)}
+                        </Link>
+                      </h1>
+                      <h2 className="my-auto font-bold text-black text-xl">
+                        {parse(description)}
+                      </h2>
+                    </div>
+                  ) : (
+                    <Link className="text-lg font-sans" to={to}>
+                      {link}
+                    </Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </header>
+
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
